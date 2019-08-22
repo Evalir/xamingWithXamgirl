67b708e2b03fb0130779673eac9fe853172238a4
Enrique Ortiz <enrique@cuyaya.com>
2019-08-22 14:56:28 -0400

    Finish class exercise

---
+++ navigationx/MyPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace navigationx
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnEntryChange(object sender, EventArgs args)
+        {
+            entryLabel.Text = (sender as Entry).Text;
+        }
+
+        public async void OnButtonClicked(object sender, EventArgs args)
+        {
+            var btn = sender as Button;
+            entryLabel.TextColor = btn.BackgroundColor;
+            await Navigation.PushAsync(new SecondPage(btn.BackgroundColor, btn.Text));
+        }
+    }
+}
