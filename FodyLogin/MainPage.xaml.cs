4a08a6a7935195401083e731e94dc2ea74abfbe8
Enrique Ortiz <enrique@cuyaya.com>
2019-08-22 15:00:47 -0400

    Finish class exercise

---
+++ FodyLogin/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FodyLogin.ViewModels;
+using Xamarin.Forms;
+
+namespace FodyLogin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+        public async void OnButtonClicked(object sender, EventArgs args)
+        {
+            await DisplayAlert("Hello!", "You've logged in successfully.", "OK");
+        }
+    }
+}
---
+++ FodyLogin/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FodyLogin.ViewModels
+{
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Notification
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(Email))
+                    return "Email cannot be empty";
+                else if (String.IsNullOrEmpty(Password))
+                    return "Password cannot be empty.";
+                else return "";
+            }
+        }
+
+        public bool ButtonEnabled
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(Email) && !String.IsNullOrEmpty(Password))
+                    return true;
+                return false;
+            }
+        }
+    }
+}
