5ddc9669b76772114ee2692c8fbdf8fabb24d8b9
Enrique Ortiz <enrique@cuyaya.com>
2019-08-22 14:53:21 -0400

    Finish first solution

---
+++ Extra1/ExercisePage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Extra1
+{
+    public class ExercisePage : ContentPage
+    {
+        public ExercisePage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ Extra1/ExercisePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Extra1
+{
+    public partial class ExercisePage : ContentPage
+    {
+        public ExercisePage()
+        {
+            InitializeComponent();
+            fBox.BackgroundColor = Color.FromRgba(255, 255, 255, 200);
+            sBox.BackgroundColor = Color.FromRgba(255, 255, 255, 220);
+            tBox.BackgroundColor = Color.FromRgba(255, 255, 255, 220);
+            foBox.BackgroundColor = Color.FromRgba(255, 255, 255, 220);
+        }
+    }
+}
